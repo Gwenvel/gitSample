af4ff5b1660bb8fb0a0d1bd91e9f2759c7e9e6c8
Gwenvel <malelkim@gmail.com>
2018-06-05 16:20:35 -0700

    changed console message.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace gitSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("my first push");
         }
     }
 }
